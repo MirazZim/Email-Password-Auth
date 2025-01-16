10d079fd32b9db5ca56d51b40dcf838415545746
MirazZim <miraz.zim.38@gmail.com>
2025-01-16 16:01:24 +0600

    Did some minor design change in sign Up page

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -11,20 +11,20 @@ const SignUp = () => {
         const email = e.target.email.value;
         const password = e.target.password.value;
 
-        console.log('Email : ',email)
-        console.log('Password : ',password)
+        console.log('Email : ', email)
+        console.log('Password : ', password)
 
         //Firebase Setup
 
-        createUserWithEmailAndPassword(auth,email,password)
-        .then (result => {
-            console.log(result.user)
-           
-        })
-        .catch(error => {
-            console.log('ERROR', error)
-            
-        })
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user)
+
+            })
+            .catch(error => {
+                console.log('ERROR', error)
+
+            })
 
 
     }
@@ -32,43 +32,33 @@ const SignUp = () => {
 
 
     return (
-        <div className="max-w-lg mx-auto">
-            <h2 className="text-4xl font-bold my-8">Sign Up</h2>
-            <form onSubmit={handleSignUp} >
-                <label className="input input-bordered flex items-center gap-2">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        className="h-4 w-4 opacity-70">
-                        <path
-                            d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-                        <path
-                            d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-                    </svg>
-                    <input type="email" name="email" className="grow" placeholder="Email" />
-                </label>
-
-                <br />
-
-                <label className=" input input-bordered flex items-center gap-2">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        className="h-4 w-4 opacity-70">
-                        <path
-                            fillRule="evenodd"
-                            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-                            clipRule="evenodd" />
-                    </svg>
-                    <input type="password" placeholder="Password" name="password" className="grow" />
-                </label>
-                <br />
-                <button className="btn btn-accent btn-wide">Login</button>
-            </form>
-            <br />
-        </div>
+        
+                
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
+                    <h3 className="text-3xl ml-9 font-bold">Sign Up Now</h3>
+                    <form onSubmit={handleSignUp} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+       
+        
     );
 };
 
